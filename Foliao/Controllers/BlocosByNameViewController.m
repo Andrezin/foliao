fde94475ba5a178ac631717a5bd2351bc1d04e30
Gustavo Barbosa <gustavocsb@gmail.com>
2012-11-17 22:18:14 -0200

    Indexed table view for Blocos By Name.

--- Foliao/Controllers/BlocosByNameViewController.m
+++ Foliao/Controllers/BlocosByNameViewController.m
@@ -11,10 +11,18 @@
 #import "BlocosByNameViewController.h"
 #import "BlocoViewController.h"
 
+#define kALPHABET @[@"A",@"B",@"C",@"D",@"E",@"F",@"G",@"H",@"I",\
+                    @"J",@"K", @"L",@"M",@"N",@"O",@"P",@"Q",@"R",\
+                    @"S",@"T",@"U",@"V",@"W",@"X",@"Y",@"Z"]
 
-@interface BlocosByNameViewController()
+
+@interface BlocosByNameViewController() {
+    NSMutableArray *_tableData;
+}
 
 @property (strong, nonatomic) NSArray *blocos;
+
+- (void)arrangeTableData;
 
 @end
 
@@ -24,24 +32,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-//    PFObject *desfileSargento = [[PFObject alloc] initWithClassName:@"Parade"];
-//    [desfileSargento setObject:[PFObject objectWithoutDataWithClassName:@"Bloco" objectId:@"3Lu1AYt7Xo"] forKey:@"bloco"];
-//    [desfileSargento setObject:[NSDate date] forKey:@"date"];
-//    
-//    PFGeoPoint *geopoint = [[PFGeoPoint alloc] init];
-//    geopoint.latitude = -22.983819;
-//    geopoint.longitude = -43.204528;
-//    [desfileSargento setObject:geopoint forKey:@"location"];
-//    [desfileSargento saveInBackground];
 
     PFQuery *query = [PFQuery queryWithClassName:@"Bloco"];
     [query orderByAscending:@"name"];
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         if (!error) {
-            // The find succeeded.
             NSLog(@"Successfully retrieved %d blocos.", objects.count);
             self.blocos = objects;
+            [self arrangeTableData];
             [self.tableView reloadData];
         } else {
             // Log details of the failure
@@ -50,16 +48,64 @@
     }];
 }
 
+- (void)arrangeTableData
+{
+    _tableData = [NSMutableArray arrayWithCapacity:kALPHABET.count];
+    
+    // Takes only letters with at least one bloco
+    NSMutableSet *lettersWithAtLeastOneBloco = [NSMutableSet set];
+    for (PFObject *bloco in self.blocos) {
+        NSString *firstLetter = [NSString stringWithFormat:@"%c", [bloco[@"name"] characterAtIndex:0]];
+        [lettersWithAtLeastOneBloco addObject:firstLetter];
+    }
+    
+    // For each letter, initialize its section
+    for (NSString *letter in lettersWithAtLeastOneBloco) {
+        [_tableData addObject:@{@"section":letter, @"rows":[NSMutableArray array]}];
+    }
+    
+    // Fill table data
+    for (PFObject *bloco in self.blocos) {
+        NSString *firstLetter = [NSString stringWithFormat:@"%c", [bloco[@"name"] characterAtIndex:0]];
+        for (NSDictionary *_sectionData in _tableData) {
+            if ([firstLetter isEqualToString:_sectionData[@"section"]]) {
+                [_sectionData[@"rows"] addObject:bloco[@"name"]];
+            }
+        }
+    }
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return _tableData.count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return _tableData[section][@"section"];
+}
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
+{
+    return kALPHABET;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
+{
+    for (int i = 0; i < _tableData.count; i++) {
+        if ([title isEqualToString:_tableData[i][@"section"]]) {
+            return i;
+        }
+    }
+    
+    return -1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.blocos.count;
+    return [_tableData[section][@"rows"] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -70,8 +116,7 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
     
-    cell.textLabel.text = self.blocos[indexPath.row][@"name"];
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.text = _tableData[indexPath.section][@"rows"][indexPath.row];
     
     return cell;
 }
