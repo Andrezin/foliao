99062575bc128107ae9984965a6a106de4cbbdd7
Gustavo Barbosa <gustavocsb@gmail.com>
2012-11-28 20:45:20 -0200

    Function to check if it is an iPhone 5

--- Foliao/AppDelegate.h
+++ Foliao/AppDelegate.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+#define IS_IPHONE_5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
+
 @class ZUUIRevealController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
