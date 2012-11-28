e6a3f2aaf264c87c98b521f07888d325cea75d52
Gustavo Barbosa <gustavocsb@gmail.com>
2012-11-28 20:44:14 -0200

    Map settings VC. Just a draft.

---
+++ Foliao/MapSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapSettingsViewController.h
+//  Foliao
+//
+//  Created by Gustavo Barbosa on 11/26/12.
+//  Copyright (c) 2012 7pixels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapSettingsViewController : UIViewController
+
+@end
